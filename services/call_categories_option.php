80157913763649ef11981e7130ae893f0d846c2f
Nguyen Duc Lam <nguyenduclam0605@gmail.com>
2020-05-20 15:25:44 +0700

    TraVinh_QuanTrac

--- services/call_categories_option.php
+++ services/call_categories_option.php
@@ -1,1 +1,18 @@
 <?php
+    include "config.php";
+?>
+
+<?php
+    /*** Querry lựa chọn loại trạm ***/
+    $querry_option_categories = 'SELECT * FROM "Category"';
+    $result = pg_query($travinh_db, $querry_option_categories);
+    if (!$result) {
+        echo "Không có dữ liệu.\n";
+        exit;
+    }
+
+    /*** Chuyển định dạng từ Array sang Geojson ***/
+    while ($row = pg_fetch_assoc($result)) {
+       var_dump(json_encode($row));
+    }
+?>
