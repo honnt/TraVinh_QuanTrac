3056b43eccc77b4e16493df9f151219f6af1fce6
Nguyen Duc Lam <nguyenduclam0605@gmail.com>
2020-05-27 08:31:17 +0700

    TraVinh_QuanTrac

--- services/call_obser_station.php
+++ services/call_obser_station.php
@@ -7,6 +7,7 @@
     Áp dụng cho trường hợp gọi xem thuộc tính bảng quan trắc và search tìm kiếm ***/
 
     /*** Select Data ***/
+    $loaihinh = $_GET['loaihinh'];
     $loaitram = $_GET['loaitram'];
     $quanhuyen = $_GET['quanhuyen'];
     $querry_tramqt_select = 'SELECT
@@ -31,24 +32,56 @@
                         LEFT JOIN "Location" "location" ON "location"."id" = "station"."locationid"
                         LEFT JOIN "District" "district" ON "district"."id" = "station"."districtid"
 						LEFT JOIN "Obstype_Station" "obs_station" ON "obs_station"."stationid" = "station"."id"
-						LEFT JOIN "ObservationType" "obs_type" ON "obs_type"."id" = "obs_station"."obstypesid"';
+						LEFT JOIN "ObservationType" "obs_type" ON "obs_type"."id" = "obs_station"."obstypesid"
+						
+						WHERE "station"."id" IN (
+						
+						SELECT "station_In"."id"
+                        FROM "Observationstation" "station_In"
+                        LEFT JOIN "Category" "category" ON "category"."id" = "station_In"."categoryid"
+                        LEFT JOIN "Organization" "organization" ON "organization"."id" = "station_In"."organizationid"
+                        LEFT JOIN "Enterprise" "enterprise" ON "enterprise"."id" = "station_In"."enterpriseid"
+                        LEFT JOIN "Basin" "basin" ON "basin"."id" = "station_In"."basinid"
+                        LEFT JOIN "Location" "location" ON "location"."id" = "station_In"."locationid"
+                        LEFT JOIN "District" "district" ON "district"."id" = "station_In"."districtid"
+			            LEFT JOIN "Obstype_Station" "obs_station" ON "obs_station"."stationid" = "station_In"."id"
+			            LEFT JOIN "ObservationType" "obs_type" ON "obs_type"."id" = "obs_station"."obstypesid"';
+
+    /*** Where Condition Data Loại hình ***/
+    $querry_tramqt_where_loaihinh = ' WHERE ';
+    if (count($loaihinh) == 1) {
+        if ($loaihinh[0] ==  0) {
+            $querry_tramqt_where_loaihinh.= '1=1 ';
+        } else {
+            $querry_tramqt_where_loaihinh.= '"obs_station"."obstypesid" = '.$loaihinh[0];
+        }
+    } else {
+        for ($i = 0; $i < count($loaihinh); $i++) {
+            if ($i == count($loaihinh) - 1) {
+                $querry_tramqt_where_loaihinh.= '"obs_station"."obstypesid" = '.$loaihinh[$i];
+            } else {
+                $querry_tramqt_where_loaihinh.= '"obs_station"."obstypesid" = '.$loaihinh[$i]." & ";
+            }
+        }
+    }
+    $querry_tramqt_where_loaihinh.= ')';
+
+    /*** Where Condition Data Loại trạm và Quận huyện (1=1 là không có điều kiện xảy ra)***/
+    $querry_tramqt_where_loaitram_quanhuyen = 'AND 1=1';
+    if ($quanhuyen != '1=1') {
+        $querry_tramqt_where_loaitram_quanhuyen.=' AND "district"."name" = '.$quanhuyen;
+    }
+    if ($loaitram != '1=1') {
+        $querry_tramqt_where_loaitram_quanhuyen.=' AND "category"."name" = '.$loaitram;
+    }
 
     /*** Group and Order Data ***/
     $querry_tramqt_group = ' GROUP BY "station"."id", "category"."name", "organization"."name", 
                             "enterprise"."name", "basin"."name", "location"."name", "district"."name"
                             ORDER BY "station"."name" ASC';
 
-    /*** Where Condition Data (1=1 là không có điều kiện xảy ra)***/
-    $querry_tramqt_where = '1=1';
-    if ($quanhuyen != '1=1') {
-        $querry_tramqt_where.=' AND "district"."name" = '.$quanhuyen;
-    }
-    if ($loaitram != '1=1') {
-        $querry_tramqt_where.=' AND "category"."name" = '.$loaitram;
-    }
-
     /*** Gộp 3 chuỗi trên tạo thành câu truy vấn ***/
-    $querry_tramqt = $querry_tramqt_select.' WHERE '.$querry_tramqt_where.$querry_tramqt_group;
+    $querry_tramqt = $querry_tramqt_select.$querry_tramqt_where_loaihinh.$querry_tramqt_where_loaitram_quanhuyen.$querry_tramqt_group;
     // echo $querry_tramqt;
     $result = pg_query($travinh_db, $querry_tramqt);
     if (!$result) {
